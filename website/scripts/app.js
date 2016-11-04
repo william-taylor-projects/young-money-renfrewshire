af091fe7cf946b79c674297b1f48f6b63e1333b4
William Taylor <wi11berto@yahoo.co.uk>
2016-11-04 23:52:07 +0000

    basic work started

--- website/scripts/app.js
+++ website/scripts/app.js
@@ -55,13 +55,40 @@ class App extends React.Component {
             <MuiThemeProvider muiTheme={muiTheme}>
                 <div>
                     <AppBar title="YMR - Young Money Renfrewshire" iconElementRight={<SideButton/>} onLeftIconButtonTouchTap={()=> this.openSidebar()} />
-                    
+
                     <div className='heading text-center'>
                         <div className="container">
                             <div className="jumbotron">
-                                <h1>Welcome to landing page!</h1>
-                                <p>This is an example for jumbotron.</p>
+                                <h1>Young Money Renfrewshire</h1>
+                                <p>Jumbo dummy text</p>
                             </div>    
+                        </div>
+                    </div>
+
+                    <div className='container-fluid'>
+                        <div className='row'>
+                            <div className='col-md-12'>
+                                <h1>What is YMR?</h1>
+                                <p>
+                                    YMR or Young Money Renfewshire is an application for under 25s who
+                                    can help with your financial problems. We can help with budgeting, 
+                                    where to find deals and where to find the best advice.
+                                    The application was built in collaboration with the Poverty Alliance,
+                                    Barnardos staff and Renfewshire Council.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className='row'>
+                            <div className='col-xs-4 text-center'>
+                                <img className='img-responsive img-center' src='http://www.scdc.org.uk/media/resources/images/news/PovertyAllianceLogo.gif' />
+                            </div>
+                            <div className='col-xs-4 text-center'>
+                                <img className='img-responsive img-center' src="https://upload.wikimedia.org/wikipedia/en/6/69/Barnardo's_logo.png" />
+                            </div>
+                            <div className='col-xs-4'>
+                                <img className='img-responsive img-center' src='https://www.myjobscotland.gov.uk/sites/default/files/styles/medium/public/organisations/Refrewshire_0.png?itok=iMO5CvRG' />
+                            </div>
                         </div>
                     </div>
 
--- website/scripts/components/sidebar.js
+++ website/scripts/components/sidebar.js
@@ -3,9 +3,27 @@ import PersonAdd from 'material-ui/svg-icons/social/person-add';
 import MenuItem from 'material-ui/MenuItem';
 import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
-
+import Subheader from 'material-ui/Subheader';
 import ReactDOM from 'react-dom';
 import React from 'react';
+
+import RaisedButton from 'material-ui/RaisedButton';
+import {List, ListItem} from 'material-ui/List';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import Divider from 'material-ui/Divider';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+
+
+import ShopIcon from 'material-ui/svg-icons/action/shopping-cart';
+import MathIcon from 'material-ui/svg-icons/action/assessment';
+import HomeIcon from 'material-ui/svg-icons/action/home';
+import NewIcon from 'material-ui/svg-icons/av/new-releases';
+import InfoIcon from 'material-ui/svg-icons/action/info';
+import TopIcon from 'material-ui/svg-icons/action/grade';
+import MapIcon from 'material-ui/svg-icons/maps/map';
 
 export default class Sidebar extends React.Component {
     constructor() {
@@ -25,16 +43,29 @@ export default class Sidebar extends React.Component {
         return (
             <Drawer docked={false} open={this.state.open} width={250} onRequestChange={(open) => this.setState({open})}>
                 <AppBar title="Menu" iconElementLeft={<div></div>} style={{"textAlign":"center"}} />
-                
-                <MenuItem primaryText='Home Page' leftIcon={<PersonAdd />}></MenuItem>
-                <MenuItem primaryText='Local Branches' leftIcon={<PersonAdd />}></MenuItem>
-                <MenuItem primaryText='Finance Calculator' leftIcon={<PersonAdd />}></MenuItem>
-                <MenuItem primaryText='Contact' leftIcon={<PersonAdd />}></MenuItem>
-                <MenuItem primaryText='F&Q' leftIcon={<PersonAdd />}></MenuItem>
-                <br/>
-                
-                <img className='logo img-rounded' src='images/council.png' />
+                <List>
+                    <Subheader>Pages</Subheader>
+                    <ListItem primaryText="Home" leftIcon={<HomeIcon />} />
+                    <ListItem primaryText="Information" leftIcon={<InfoIcon />} />
+                    <ListItem primaryText="Whats New?" leftIcon={<NewIcon />} />
+                    <ListItem primaryText="Good Deals" leftIcon={<ShopIcon />} />
+                    <ListItem primaryText="Top Tips" leftIcon={<TopIcon />} />
+                </List>
+                <Divider />
+                <List>
+                    <Subheader>Tools</Subheader>
+                    <ListItem primaryText="Finance Calculator" leftIcon={<MathIcon />} />
+                    <ListItem primaryText="Bank Map" leftIcon={<MapIcon />} />
+                </List>
+                <Divider />
+                <List>
+                    <Subheader>Help</Subheader>
+                    <ListItem primaryText="Contact Us" rightIcon={<ActionInfo />} />
+                    <ListItem primaryText="F&Q" rightIcon={<ActionInfo />} />
+                </List>          
             </Drawer>
         );
     }
 }
+
+// <img className='logo img-rounded' src='images/council.png' />
