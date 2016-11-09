60c9d950c4e1c935177c3d139f79c3aaf25525dc
William Taylor <wi11berto@yahoo.co.uk>
2016-11-09 19:29:00 +0000

    adding electron support

---
+++ website/electron-entry.js
@@ -0,0 +1,37 @@
+
+const electron = require('electron');
+const app = electron.app;
+const settings = {
+  center: true,
+  width: 1280,
+  height: 800,
+  resizable: false,
+  icon: `${__dirname}/images/council.png`,
+  webPreferences: {
+    webSecurity: false
+  }
+};
+
+let mainWindow = null;
+
+function createWindow() {
+  mainWindow = new electron.BrowserWindow(settings);
+  mainWindow.loadURL(`file:///${__dirname}/index.html`);
+  mainWindow.setMenu(null);
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow)
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
