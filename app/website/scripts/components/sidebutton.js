912b7862d2b9a6ff1fca01b3249586ea1498dbd2
William Taylor <wi11berto@yahoo.co.uk>
2017-02-12 13:46:34 +0000

    latest build with links properly fixed #21

--- app/website/scripts/components/sidebutton.js
+++ app/website/scripts/components/sidebutton.js
@@ -5,7 +5,7 @@ import MenuItem from 'material-ui/MenuItem';
 import IconMenu from 'material-ui/IconMenu';
 import React from 'react';
 
-const open = url => window.open(url, '_blank');
+const open = url => window.open(url, '_system');
 
 export default class SideButton extends React.Component {
     render() {
@@ -13,15 +13,9 @@ export default class SideButton extends React.Component {
             <IconMenu iconButtonElement={<IconButton><MoreVertIcon /></IconButton> }
                 targetOrigin={{horizontal: 'right', vertical: 'top'}}
                 anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
-                <a className='nounderline' target='_system' href='http://www.renfrewshire.gov.uk/'>
-                    <MenuItem primaryText="Renfewshire Council" />
-                </a>
-                <a className='nounderline' target='_system' href='http://www.barnardos.org.uk/'>
-                    <MenuItem primaryText="Barnardos" />
-                </a>
-                <a className='nounderline' target='_system' href='http://www.ymcascotland.org/locations/paisley/'>
-                    <MenuItem primaryText="YMCA" />
-                </a>
+                <MenuItem onTouchTap={() => open('http://www.renfrewshire.gov.uk/')} primaryText="Renfewshire Council" />
+                <MenuItem onTouchTap={() => open('http://www.barnardos.org.uk/')} primaryText="Barnardos" />
+                <MenuItem onTouchTap={() => open('http://www.ymcascotland.org/locations/paisley/')} primaryText="YMCA" />
             </IconMenu>
         );
     }
