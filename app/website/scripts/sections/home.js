7f5cd7e587cd36206ed975e35ee4cf24a9cbd932
William Taylor <wi11berto@yahoo.co.uk>
2017-02-12 11:40:06 +0000

    fixing typos

--- app/website/scripts/sections/home.js
+++ app/website/scripts/sections/home.js
@@ -20,7 +20,7 @@ export default class Home extends React.Component {
                         <div className='col-md-12'>
                             <h1>What is YMR?</h1><hr/>
                             <p>
-                                YMR or Young Money Renfrewshire is an application for under youngsters 25s who need help with financial problems or questions. We can help with budgeting, where to find deals and where to find the best advice. This application was built in collaboration with the Poverty Alliance, Barnardo’s staff and Renfrewshire Council.
+                                YMR or Young Money Renfrewshire is an application for young adults under 25 who need help with financial problems or questions. We can help with budgeting, where to find deals and where to find the best advice. This application was built in collaboration with the Poverty Alliance, Barnardo’s staff and Renfrewshire Council.
                             </p>
                             <br/>
                         </div>
