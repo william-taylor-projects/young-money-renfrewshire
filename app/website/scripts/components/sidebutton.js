4dbed0a6d777561fa97196dba4d94a4267c7cee5
william-taylor <wi11berto@yahoo.co.uk>
2017-04-25 23:07:56 +0100

    fixing sidebutton text

--- app/website/scripts/components/sidebutton.js
+++ app/website/scripts/components/sidebutton.js
@@ -16,7 +16,7 @@ export default class SideButton extends React.Component {
                 <MenuItem onTouchTap={() => open('http://www.investinrenfrewshire.com/')} primaryText="Invest in Renfrewshire" />
                 <MenuItem onTouchTap={() => open('http://www.renfrewshire.gov.uk/')} primaryText="Renfewshire Council" />
                 <MenuItem onTouchTap={() => open('http://www.barnardos.org.uk/')} primaryText="Barnardos" />
-                <MenuItem onTouchTap={() => open('http://www.ymcascotland.org/locations/paisley/')} primaryText="YMCA" />
+                <MenuItem onTouchTap={() => open('http://www.ymcascotland.org/locations/paisley/')} primaryText="YMCA Paisley" />
             </IconMenu>
         );
     }
